387d7167fa21e857b07eae095e7b272dfda06b2d
Anthony Petrov <beavean@gmail.com>
2022-06-16 20:33:25 +0300

    initial commit

--- Philter/ViewController.swift
+++ Philter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var intensity: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
 }
 
